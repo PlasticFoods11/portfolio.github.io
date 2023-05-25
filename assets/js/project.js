42d4b7bb6b17073acc5c5e44b590a003e18a5942
JuliaNGaminG007 <julianaaron1970@gmail.com>
2023-05-25 17:49:18 +0530

    Added a new project to project page

--- assets/js/project.js
+++ assets/js/project.js
@@ -67,8 +67,16 @@ const projects = [
     cardImage: "assets/images/project-page/translator.png",
     description:  "Simple Yet Powerful QR Code Generator.",
     tagimg: "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
-    Previewlink: "http://qr-code-generator.julianidl.repl.co/",
-    Githublink: "https://github.com/Julian-Idl/QR-Code-Generator",
+    Previewlink: "https://translator.julianidl.repl.co/",
+    Githublink: "https://github.com/Julian-Idl/LanguageTranslator",
+  },
+  {
+    title: "TextToSpeech",
+    cardImage: "assets/images/project-page/6_m_1_efec69ffb1.png",
+    description:  "A project that converts any text into audio speech",
+    tagimg: "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
+    Previewlink: "https://text2speech.julianidl.repl.co/",
+    Githublink: "https://github.com/Julian-Idl/TextToSpeech",
   },
   {
     title: "QR Code Generator",
